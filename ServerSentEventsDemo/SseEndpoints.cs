5f466fdfe9298de33d86c7849a95052e635b7173
stephane.adjakotan@engie.com <BS6655@engie.com>
2025-10-05 20:37:49 +0200

    refactor

--- ServerSentEventsDemo/SseEndpoints.cs
+++ ServerSentEventsDemo/SseEndpoints.cs
@@ -13,9 +13,10 @@ public static class SseEndpoints
         sseGroup.MapGet("/servers", async (
                 HttpContext ctx,
                 IEventStreamingService<ServerResponse> service,
+                SseStreamer streamer,
                 CancellationToken cancellationToken) =>
             {
-                await StreamEventsAsync(ctx, service, cancellationToken);
+                await streamer.StreamEventsAsync(ctx, service, cancellationToken);
             })
             .WithName("ServerEvents")
             .WithSummary("Stream server events (created, updated, deleted)")
@@ -25,57 +26,13 @@ public static class SseEndpoints
         sseGroup.MapGet("/clients", async (
                 HttpContext ctx,
                 IEventStreamingService<ClientResponse> service,
+                SseStreamer streamer,
                 CancellationToken cancellationToken) =>
             {
-                await StreamEventsAsync(ctx, service, cancellationToken);
+                await streamer.StreamEventsAsync(ctx, service, cancellationToken);
             })
             .WithName("ClientEvents")
             .WithSummary("Stream client events (created, updated, deleted)")
             .WithOpenApi();
-    }
-
-    private static async Task StreamEventsAsync<TDto>(
-        HttpContext ctx,
-        IEventStreamingService<TDto> service,
-        CancellationToken cancellationToken) where TDto : IBaseDto
-    {
-        ctx.Response.Headers.Append(HeaderNames.ContentType, "text/event-stream");
-        ctx.Response.Headers.Append(HeaderNames.CacheControl, "no-cache");
-        ctx.Response.Headers.Append("Connection", "keep-alive");
-
-        try
-        {
-            while (await service.Events.WaitToReadAsync(cancellationToken))
-            {
-                var broadcastMessage = await service.Events.ReadAsync(cancellationToken);
-
-                var eventData = new
-                {
-                    type = broadcastMessage.MessageType.ToString().ToLowerInvariant(),
-                    data = broadcastMessage.Dto,
-                    timestamp = DateTimeOffset.UtcNow
-                };
-
-                var json = JsonSerializer.Serialize(eventData, new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                });
-
-                await ctx.Response.WriteAsync($"data: {json}\n\n", cancellationToken);
-                await ctx.Response.Body.FlushAsync(cancellationToken);
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            // Client disconnected - normal
-        }
-        catch (Exception ex)
-        {
-            // Client disconnected - normal
-        }
-        finally
-        {
-            await ctx.Response.CompleteAsync();
-        }
     }
 }
